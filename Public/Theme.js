b60859c8db8cd33441a0dbbcb7624314485c25e4
Luca spopo <katoch.anirudh@gmail.com>
2016-11-19 11:55:20 +0530

    Added overlayed theme

--- Public/Theme.js
+++ Public/Theme.js
@@ -13,30 +13,58 @@ var Theme;
 		let ctx = this.ctx2D
 		ctx.save();
 		// Use the identity matrix while clearing the canvas
-		ctx.setTransform(1, 0, 0, 1, 0, 0);
-		ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+	ctx.globalCompositeOperation = "source-over";
+	//Lets reduce the opacity of the BG paint to give the final touch
+	ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+		///////////////////
+				ctx.setTransform(1, 0, 0, 1, 0, 0);
+		////////////////////
+
+		ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 		// Restore the transform
 		ctx.restore();
+
+		///////////////
+	//Lets blend the particle with the BG
+	ctx.globalCompositeOperation = "lighter";
+
+		//////////////
 
 		ctx.strokeStyle = "#FFFF00"
 		for (const keyval of visible.players) {
 			const player = keyval[1]
-			//console.log("My id is "+player.localid+" and "+player.name+"'s id is "+player.hislocalid)
-			if (player.hislocalid == player.localid)
-				ctx.fillStyle = "#227722";
-			else if (player.getRelation(player.localid, player.hislocalid))
-				ctx.fillStyle = "#444477";
-			else
-				ctx.fillStyle = "#FF3333";
-			ctx.beginPath()
-			// console.log(player.id == player.myId, player.hunted)
 			var arr = player.getXY(Date.now()-30)
 			var px = arr[0]
 			var py = arr[1]
+			
+			var color
+			///////////
+		var gradient = ctx.createRadialGradient(px, py, 0, px, py, player.radius);
+		gradient.addColorStop(0, "white");
+		gradient.addColorStop(0.4, "white");
+			/////////
+			//console.log("My id is "+player.localid+" and "+player.name+"'s id is "+player.hislocalid)
+			if (player.hislocalid == player.localid)
+				color = "#227722";
+			else if (player.getRelation(player.localid, player.hislocalid))
+				color = "#444477";
+			else
+				color = "#FF3333";
+			//////////////////
+			gradient.addColorStop(0.4, color);
+			gradient.addColorStop(1, "black");
+		ctx.fillStyle = gradient;
+		
+			////////////////
+			ctx.beginPath()
+			// console.log(player.id == player.myId, player.hunted)
 			//console.log(px, py)
 			ctx.arc(px, py, player.radius, 0, 100)
 			ctx.closePath()
 			ctx.fill()
+			ctx.fillStyle = color;
+		
 			ctx.fillText(player.name, px-player.radius, py-5-player.radius);
 		}
 	}
